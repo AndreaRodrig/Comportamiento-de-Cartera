1dcca4d9afe5033a16232abec7e0bbf16f7a0c68
Andrea Rodríguez <108045999+AndreaRodrig@users.noreply.github.com>
2025-12-18 12:50:28 -0600

    Create cobranza.py

---
+++ cobranza.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from statsmodels.tsa.seasonal import seasonal_decompose
+from sklearn.preprocessing import LabelEncoder
+import os
